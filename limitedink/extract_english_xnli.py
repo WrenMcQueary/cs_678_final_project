2b4b2f320b2c08476a6d3520a7897e60bd4772d6
Wren McQueary <wren.mcqueary@gmail.com>
2023-04-07 14:36:11 -0400

    Split dev data into a training set (80%) and a validation set (20%)

--- limitedink/extract_english_xnli.py
+++ limitedink/extract_english_xnli.py
@@ -1,27 +1,40 @@
-"""For our baseline report.
-Extract the English-language examples from XNLI dataset, which
+"""Extract the English-language examples from XNLI dataset, which
 can be used as a stepping stone to getting XNLI working with 
 LimitedInk
 """
 
 import jsonlines
+import random
+random.seed(0)
 
-# Create english dev data set
-count = 0
+# Create english train and val data sets
+train_ratio = 0.8   # How much of the dev dataset to turn into training data.  The rest will be val data.
+train_count = 0
+val_count = 0
+dev_count = 0
 with jsonlines.open("../data/XNLI-1.0/xnli.dev.jsonl", 'r') as reader:
-    with jsonlines.open("../data/XNLI-1.0/xnli.dev.en.jsonl", mode='w') as writer:
-        for obj in reader:
-            if obj['language'] == 'en':
-                count += 1
-                writer.write(obj)
-print(f"Number of english dev examples: {count}")
+    english_objs = [obj for obj in reader if obj['language'] == 'en']
+random.shuffle(english_objs)
+with jsonlines.open("../data/XNLI-1.0/xnli.train.en.jsonl", mode='w') as writer:
+    for obj in english_objs[:round(len(english_objs)*train_ratio)]:
+        train_count += 1
+        dev_count += 1
+        writer.write(obj)
+with jsonlines.open("../data/XNLI-1.0/xnli.val.en.jsonl", mode='w') as writer:
+    for obj in english_objs[round(len(english_objs)*train_ratio):]:
+        val_count += 1
+        dev_count += 1
+        writer.write(obj)
+print(f"Number of english train examples: {train_count}")
+print(f"Number of english val examples: {val_count}")
+print(f"Number of english dev examples (train+val): {dev_count}")
 
 # Create english test data set
-count = 0
+test_count = 0
 with jsonlines.open("../data/XNLI-1.0/xnli.test.jsonl", 'r') as reader:
     with jsonlines.open("../data/XNLI-1.0/xnli.test.en.jsonl", mode='w') as writer:
         for obj in reader:
             if obj['language'] == 'en':
-                count += 1
+                test_count += 1
                 writer.write(obj)
-print(f"Number of english test examples: {count}")
+print(f"Number of english test examples: {test_count}")
