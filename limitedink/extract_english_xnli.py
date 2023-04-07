345bdabe53eab2f3ed44f52bba135778883bfc99
Wren McQueary <wren.mcqueary@gmail.com>
2023-04-07 15:16:35 -0400

    Renamed xnli.train.en.jsonl to train.jsonl.  Similar renamings for val and test

--- limitedink/extract_english_xnli.py
+++ limitedink/extract_english_xnli.py
@@ -15,12 +15,12 @@ dev_count = 0
 with jsonlines.open("../data/XNLI-1.0/xnli.dev.jsonl", 'r') as reader:
     english_objs = [obj for obj in reader if obj['language'] == 'en']
 random.shuffle(english_objs)
-with jsonlines.open("../data/XNLI-1.0/xnli.train.en.jsonl", mode='w') as writer:
+with jsonlines.open("../data/XNLI-1.0/train.jsonl", mode='w') as writer:
     for obj in english_objs[:round(len(english_objs)*train_ratio)]:
         train_count += 1
         dev_count += 1
         writer.write(obj)
-with jsonlines.open("../data/XNLI-1.0/xnli.val.en.jsonl", mode='w') as writer:
+with jsonlines.open("../data/XNLI-1.0/val.jsonl", mode='w') as writer:
     for obj in english_objs[round(len(english_objs)*train_ratio):]:
         val_count += 1
         dev_count += 1
@@ -32,7 +32,7 @@ print(f"Number of english dev examples (train+val): {dev_count}")
 # Create english test data set
 test_count = 0
 with jsonlines.open("../data/XNLI-1.0/xnli.test.jsonl", 'r') as reader:
-    with jsonlines.open("../data/XNLI-1.0/xnli.test.en.jsonl", mode='w') as writer:
+    with jsonlines.open("../data/XNLI-1.0/test.jsonl", mode='w') as writer:
         for obj in reader:
             if obj['language'] == 'en':
                 test_count += 1
